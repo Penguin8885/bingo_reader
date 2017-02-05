d93a4fb31314da70ea541edd1ada7feee70e8999
Penguin8885 <penguin.8885@gmail.com>
2017-02-06 01:03:11 +0900

    add functions to show a image and write(save) images

--- module_img_analyzer.py
+++ module_img_analyzer.py
@@ -21,26 +21,51 @@ def get_rectcontour_img(img, binary_img):
     _, contours, _ = cv2.findContours(binary_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     cnt_info_tuple_list = []
+    noisy_rectcontour_img = np.copy(img)
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
-        ratio = w/h
-        area = w*h
-        if (ratio > 0.7 and ratio < 1.3) and (area > 80000 and area < 500000):
-            cnt_info_tuple_list.append((x, y, x+w, y+h))
+        cnt_info_tuple_list.append((x, y, w, h))
+        cv2.rectangle(noisy_rectcontour_img, (x,y), (x+w,y+h), (0,0,255), 10)
+
+    passer = []
+    for cnt in cnt_info_tuple_list:
+        ratio = cnt[2] / cnt[3]
+        area = cnt[2] * cnt[3]
+        if (ratio > 0.7 and ratio < 1.3) and (area > 70000 and area < 200000):
+            passer.append((cnt))
+    cnt_info_tuple_list = passer
+
+    cnt_info_tuple_list.sort(key=lambda tuple_: tuple_[2]*tuple_[3], reverse=True)
+    for cnt in cnt_info_tuple_list:
+        print(cnt_info_tuple_list.index(cnt)+1, "\t", cnt[2]*cnt[3])
 
     rectcontour_img = np.copy(img)
     for cnt in cnt_info_tuple_list:
-        cv2.rectangle(rectcontour_img, (cnt[0],cnt[1]), (cnt[2],cnt[3]), (0,0,255), 10)
+        cv2.rectangle(rectcontour_img, (cnt[0],cnt[1]), (cnt[0]+cnt[2],cnt[1]+cnt[3]), (0,0,255), 10)
 
-    return rectcontour_img
+    return noisy_rectcontour_img, rectcontour_img
 
-if __name__ == '__main__':
-    file_name = "./data/3001-3200/IMG_4798.jpg"
+def show_rectcontour_img(file_name):
     img = cv2.imread(file_name)
 
     threshold_img = get_thresholding_img(img)
-    rectcontour_img = get_rectcontour_img(img, threshold_img)
+    _, rectcontour_img = get_rectcontour_img(img, threshold_img)
 
     rectcontour_img = cv2.cvtColor(rectcontour_img, cv2.COLOR_BGR2RGB)
     plt.imshow(rectcontour_img)
     plt.show()
+
+def write_imgs(file_name, output_name):
+    img = cv2.imread(file_name)
+
+    threshold_img = get_thresholding_img(img)
+    noisy_rectcontour_img, rectcontour_img = get_rectcontour_img(img, threshold_img)
+
+    cv2.imwrite("0_" + output_name, threshold_img)
+    cv2.imwrite("1_" + output_name, noisy_rectcontour_img)
+    cv2.imwrite("2_" + output_name, rectcontour_img)
+
+
+if __name__ == '__main__':
+    show_rectcontour_img("./data/3001-3200/IMG_4798.jpg")
+    write_imgs("./data/3001-3200/IMG_4798.jpg", "sample.jpg")
